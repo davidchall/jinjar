bc3970cb2152df2cfce3d0b440bb57983f499b68
David C Hall <davidchall@users.noreply.github.com>
2021-09-02 20:14:40 -0700

    Support alternative loaders (#1)

---
+++ src/loader.cpp
@@ -0,0 +1,60 @@
+#include "loader.h"
+#include <cpp11.hpp>
+#include <inja.hpp>
+
+
+// Loader factory ----------------------------------------------
+Loader* Loader::make_loader(const cpp11::list& config) {
+  if (Rf_isNull(config["loader"])) {
+    return new NullLoader();
+  }
+
+  const cpp11::list loader = config["loader"];
+
+  if (Rf_inherits(loader, "path_loader")) {
+    return new PathLoader(loader);
+  } else if (Rf_inherits(loader, "list_loader")) {
+    return new ListLoader(loader);
+  } else {
+    cpp11::stop("Unrecognized loader object.");
+  }
+}
+
+
+// NullLoader ---------------------------------------------------
+inja::Environment NullLoader::init_environment() {
+  inja::Environment env;
+  env.set_search_included_templates_in_files(false);
+  return(env);
+}
+
+
+// PathLoader ---------------------------------------------------
+PathLoader::PathLoader(const cpp11::list& loader) {
+  path = cpp11::as_cpp<std::string>(loader["path"]);
+}
+
+inja::Environment PathLoader::init_environment() {
+  return(inja::Environment(path));
+}
+
+
+// ListLoader ---------------------------------------------------
+ListLoader::ListLoader(const cpp11::list& loader) {
+  const cpp11::strings names = loader.names();
+  for (auto it=names.begin(); it!=names.end(); ++it) {
+    templates.push_back(std::make_pair(*it, cpp11::as_cpp<std::string>(loader[*it])));
+  }
+}
+
+inja::Environment ListLoader::init_environment() {
+  inja::Environment env;
+  env.set_search_included_templates_in_files(false);
+
+  for (auto it=templates.begin(); it!=templates.end(); ++it) {
+    inja::Template x = env.parse(it->second);
+    env.include_template(it->first, x);
+  }
+
+  return(env);
+}
--- src/render.cpp
+++ src/render.cpp
@@ -1,44 +1,6 @@
 #include <cpp11.hpp>
 #include <inja.hpp>
-
-
-inja::Environment init_environment(const cpp11::list& config) {
-  if (!Rf_inherits(config, "rinja_engine_config")) {
-    cpp11::stop("Found invalid engine config.");
-  }
-
-  bool disable_search = Rf_isNull(config["search_path"]);
-  std::string search_path = disable_search ? "" : cpp11::as_cpp<std::string>(config["search_path"]);
-
-  inja::Environment env(search_path);
-  if (disable_search) {
-    env.set_search_included_templates_in_files(false);
-  }
-
-  env.set_statement(
-    cpp11::as_cpp<std::string>(config["block_open"]),
-    cpp11::as_cpp<std::string>(config["block_close"])
-  );
-  env.set_line_statement(
-    cpp11::as_cpp<std::string>(config["line_statement"])
-  );
-  env.set_expression(
-    cpp11::as_cpp<std::string>(config["variable_open"]),
-    cpp11::as_cpp<std::string>(config["variable_close"])
-  );
-  env.set_comment(
-    cpp11::as_cpp<std::string>(config["comment_open"]),
-    cpp11::as_cpp<std::string>(config["comment_close"])
-  );
-  env.set_trim_blocks(
-    cpp11::as_cpp<bool>(config["trim_blocks"])
-  );
-  env.set_lstrip_blocks(
-    cpp11::as_cpp<bool>(config["lstrip_blocks"])
-  );
-
-  return env;
-}
+#include "config.h"
 
 
 [[cpp11::register]]
@@ -46,7 +8,7 @@ cpp11::strings c_render(const cpp11::strings& input,
                         const cpp11::strings& data_json,
                         const cpp11::list& config) {
 
-  inja::Environment env = init_environment(config);
+  inja::Environment env = setup_environment(config);
 
   auto data = nlohmann::json::parse(
     cpp11::as_cpp<std::string>(data_json)
