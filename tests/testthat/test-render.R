fc6ec8b29c69db0735314f9717b61e317c197a4f
David Hall <david.hall.physics@gmail.com>
2021-09-09 06:25:26 -0700

    Remove external libraries from coverage

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -1,3 +1,10 @@
 test_that("input validation works", {
   expect_error(render())
 })
+
+test_that("templating features work", {
+  expect_equal(
+    render("Hello {{ name }}!", list(name = "world")),
+    "Hello world!"
+  )
+})
