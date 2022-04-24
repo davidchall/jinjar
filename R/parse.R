f3ab09c93d984ef8d066bc9c57208cbea45deacb
David C Hall <davidchall@users.noreply.github.com>
2022-04-24 11:08:39 -0700

    Add parse_template()

---
+++ R/parse.R
@@ -0,0 +1,66 @@
+#' Parse template
+#'
+#' Sometimes you want to render multiple copies of a template, using different
+#' sets of data variables. [parse_template()] returns an intermediate version
+#' of the template, so you can [render()] repeatedly without re-parsing the
+#' template syntax.
+#'
+#' @param .x The template. Choices:
+#' * A template string.
+#' * A path to a template file (use [fs::path()]).
+#' @inheritParams render
+#' @return A `"jinjar_template"` object.
+#'
+#' @seealso
+#' * [render()] to render the final document using data variables.
+#' * `vignette("template-syntax")` describes how to write templates.
+#' @examples
+#' x <- parse_template("Hello {{ name }}!")
+#' render(x, name = "world")
+#' @name parse
+#' @export
+parse_template <- function(.x, .config) {
+  UseMethod("parse_template")
+}
+
+#' @rdname parse
+#' @export
+parse_template.character <- function(.x, .config = default_config()) {
+  checkmate::assert_string(.x, min.chars = 1)
+  checkmate::assert_class(.config, "jinjar_config")
+
+  structure(
+    .x,
+    config = .config,
+    parsed = parse_(.x, .config),
+    class = "jinjar_template"
+  )
+}
+
+#' @rdname parse
+#' @export
+parse_template.fs_path <- function(.x, .config = default_config()) {
+  read_utf8 <- function(path) {
+    paste(readLines(path, encoding = "UTF-8", warn = FALSE), collapse = "\n")
+  }
+
+  checkmate::assert_string(.x, min.chars = 1)
+  checkmate::assert_class(.config, "jinjar_config")
+
+  if (inherits(.config$loader, "path_loader")) {
+    if (!fs::path_has_parent(.x, .config$loader$path)) {
+      .x <- fs::path_abs(.x, .config$loader$path)
+    }
+  }
+
+  checkmate::assert_file_exists(.x, access = "r")
+
+  parse_template(read_utf8(.x), .config)
+}
+
+#' @export
+print.jinjar_template <- function(x, ...) {
+  cat(x)
+
+  invisible(x)
+}
--- src/render.cpp
+++ src/render.cpp
@@ -3,10 +3,17 @@
 
 
 [[cpp11::register]]
-cpp11::strings c_render_string(const cpp11::strings& input,
-                               const cpp11::strings& data_json,
-                               const cpp11::list& config) {
+cpp11::external_pointer<jinjar::Template> parse_(const cpp11::strings& input,
+                                                 const cpp11::list& config) {
 
-  auto tmpl = jinjar::Template(input, config);
-  return tmpl.render(data_json);
+  auto p_tmpl = new jinjar::Template(input, config);
+
+  return cpp11::external_pointer<jinjar::Template>(p_tmpl);
+}
+
+[[cpp11::register]]
+cpp11::strings render_(cpp11::external_pointer<jinjar::Template> input,
+                       const cpp11::strings& data_json) {
+
+  return input->render(data_json);
 }
